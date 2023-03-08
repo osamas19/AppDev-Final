7e7203dc09018d570a5984d358fd936c140377f4
osamas19 <osama.dogorshom@gmail.com>
2023-03-08 13:36:52 +0000

    Merchant Lookup update

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -64,13 +64,20 @@ class WalletController < ApplicationController
     #Parsing API response to get MCC
     parsed_result = JSON.parse(response)
     search_merchant = parsed_result.fetch("merchantSearchServiceResponse")
-    search_response = search_merchant.fetch("response").at(0)
-    search_response_value = search_response.fetch("responseValues")
-    merchant_category_desc = search_response_value.fetch("merchantCategoryCodeDesc")
-    primary_mcc = search_response_value.fetch("primaryMerchantCategoryCode")
-    all_mcc = search_response_value.fetch("merchantCategoryCode")
-    visa_merchant_name = search_response_value.fetch("visaMerchantName")
-    category_array = all_mcc
+
+    #checking if merchant has category 
+    if search_merchant.include? "response"
+      search_response = search_merchant.fetch("response").at(0)
+      search_response_value = search_response.fetch("responseValues")
+      merchant_category_desc = search_response_value.fetch("merchantCategoryCodeDesc")
+      primary_mcc = search_response_value.fetch("primaryMerchantCategoryCode")
+      all_mcc = search_response_value.fetch("merchantCategoryCode")
+      visa_merchant_name = search_response_value.fetch("visaMerchantName")
+      category_array = all_mcc
+    else
+      category_array = nil
+    end
+
 
     #Getting user associated cards from User_cards table
     the_user_id = session.fetch(:user_id)
@@ -83,44 +90,54 @@ class WalletController < ApplicationController
     
     #Finding the card with the highet cashback
     result_array = Array.new
-    category_array.each do |a_category|
-      Card.where({ :id => card_id_array }).each do |a_card|
-        number_of_cartegories = a_card.no_of_cats
-        if number_of_cartegories == 999
-          the_cashback = a_card.cashback
-          the_card = a_card.card_name
-        elsif a_category = a_card.cat1
-          the_cashback = a_card.cat1_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat1
-        elsif a_category = a_card.cat2
-          the_cashback = a_card.cat2_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat2
-        elsif a_category = a_card.cat3
-          the_cashback = a_card.cat3_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat3
-        elsif a_category = a_card.cat4
-          the_cashback = a_card.cat4_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat4
-        elsif a_category = a_card.cat5
-          the_cashback = a_card.cat5_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat5
-        elsif a_category = a_card.cat6
-          the_cashback = a_card.cat6_cashback
-          the_card = a_card.card_name
-          the_category = a_card.cat6
-        else 
-          the_cashback = a_card.cashback
-          the_card = a_card.card_name        
+
+    if category_array != nil
+      category_array.each do |a_category|
+        Card.where({ :id => card_id_array }).each do |a_card|
+          number_of_cartegories = a_card.no_of_cats
+          if number_of_cartegories == 999
+            the_cashback = a_card.cashback
+            the_card = a_card.card_name
+          elsif a_category = a_card.cat1
+            the_cashback = a_card.cat1_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat1
+          elsif a_category = a_card.cat2
+            the_cashback = a_card.cat2_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat2
+          elsif a_category = a_card.cat3
+            the_cashback = a_card.cat3_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat3
+          elsif a_category = a_card.cat4
+            the_cashback = a_card.cat4_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat4
+          elsif a_category = a_card.cat5
+            the_cashback = a_card.cat5_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat5
+          elsif a_category = a_card.cat6
+            the_cashback = a_card.cat6_cashback
+            the_card = a_card.card_name
+            the_category = a_card.cat6
+          else 
+            the_cashback = a_card.cashback
+            the_card = a_card.card_name        
+          end
+          card_result = Hash.new
+          card_result.store(:cashbak, the_cashback)
+          card_result.store(:card_name, the_card)
+          card_result.store(:category, the_category)
+          result_array.push(card_result)
         end
+      end
+    else
+      Card.where({ :id => card_id_array }).each do |a_card|
         card_result = Hash.new
-        card_result.store(:cashbak, the_cashback)
-        card_result.store(:card_name, the_card)
-        card_result.store(:category, the_category)
+        card_result.store(:cashbak, a_card.cashback)
+        card_result.store(:card_name, a_card.card_name)
         result_array.push(card_result)
       end
     end
